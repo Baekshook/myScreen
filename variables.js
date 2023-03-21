abc62a5266c3c3063e63e8e37494606c18bad51b
Baekshook <ecgo0106@naver.com>
2023-03-21 17:39:54 +0900

    array, object

--- variables.js
+++ variables.js
@@ -1,18 +1,31 @@
-const TRUE = true;
-const FALSE = false;
+const chains = [
+  "mainnet",
+  "polygon",
+  "avalanche",
+  "arbitrum",
+  "gnosis",
+  "bsc",
+  "optimism",
+  "fantom",
+];
 
-// && AND
-console.log(TRUE && TRUE);
-console.log(TRUE && FALSE);
-console.log(FALSE && TRUE);
-console.log(FALSE && FALSE);
+console.log(chains);
+console.log(chains[0]);
 
-// || OR
-console.log(TRUE || TRUE);
-console.log(TRUE || FALSE);
-console.log(FALSE || TRUE);
-console.log(FALSE || FALSE);
+const Mainchains = [
+  { name: "mainnet", chainId: 1 },
+  { name: "polygon", chainId: 137 },
+  { name: "avalanche", chainId: 43114 },
+  { name: "arbitrum", chainId: 42161 },
+  { name: "gnosis", chainId: 100 },
+  { name: "bsc", chainId: 56 },
+  { name: "optimism", chainId: 10 },
+  { name: "fantom", chainId: 250 },
+];
 
-// ! NOT
-console.log(!TRUE);
-console.log(!FALSE);
+console.log(Mainchains);
+console.log(Mainchains[0]);
+
+// 여전히 const 임에도 불구하고 접근이 가능한 문제가 남아있습니다.
+// chains[0].name = "hackednet";
+// console.log(chains[0]);
