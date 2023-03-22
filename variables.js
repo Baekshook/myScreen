459590feb578b47f037eec3a03e6534a474dff06
Baekshook <ecgo0106@naver.com>
2023-03-22 14:25:17 +0900

    calculator upgrade2

--- variables.js
+++ variables.js
@@ -1,20 +1,36 @@
-function add(a, b) {
-  console.log(a + b);
+function add_1(a, b) {
+  return a + b;
 }
 
-function sub(a, b) {
-  console.log(a - b);
+function sub_1(a, b) {
+  return a - b;
 }
 
-function mul(a, b) {
-  console.log(a * b);
+function mul_1(a, b) {
+  return a * b;
 }
 
-function div(a, b) {
-  console.log(a / b);
+function div_1(a, b) {
+  return a / b;
 }
 
-add(2, 3);
-sub(5,9);
-mul(7,5);
-div(10,2);
+let result = mul_1(4, 3);
+console.log(result);
+
+const calculator = {
+  add: function (num1, num2) {
+    return num1 + num2;
+  },
+  sub: function (num1, num2) {
+    return num1 - num2;
+  },
+  mul: function (num1, num2) {
+    return num1 * num2;
+  },
+  div: function (num1, num2) {
+    return num1 / num2;
+  },
+};
+
+let resultCal = calculator.sub(3, 4);
+console.log(resultCal);
